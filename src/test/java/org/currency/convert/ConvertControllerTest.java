eb958ffde0e160045780f0ff3c99186feebc6b08
Flaviu-Dorin TOADER <flaviu.toader@amadeus.com>
2018-07-30 11:29:46 +0200

    Fixed integration test

--- src/test/java/org/currency/convert/ConvertControllerTest.java
+++ src/test/java/org/currency/convert/ConvertControllerTest.java
@@ -9,12 +9,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ConvertController.class)
+@AutoConfigureRestDocs(outputDir = "target/snippets")
 public class ConvertControllerTest {
 
     @Autowired
